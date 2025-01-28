e0d59ec79fb96fbb2c268023f3a7b4a9bd7a8826
Ruth Farmery <rfarmery@phacs.org>
2025-01-28 07:33:50 -0500

    new

--- ImageEdit/Main.java
+++ ImageEdit/Main.java
@@ -1,12 +1,5 @@
 package ImageEdit;
 
-//import all the libraries
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.*;
-import java.io.*;
-import javax.imageio.*;
-
 public class Main
 {
    public static void main(String[] args) {
